32c9d31fb22047d88c5a3d3503d01cab919b2fa2
Chris Lien <clien@sharpencx.com>
2020-09-30 13:46:46 -0400

    added index.js and server file. Installed dependencies

---
+++ index.js
@@ -0,0 +1,4 @@
+require('dotenv').config();
+const server = require('./api/server.js');
+const port = process.env.PORT || 8000;
+server.listen(port, () => {console.log(`Server is listening on port ${port}`)})
